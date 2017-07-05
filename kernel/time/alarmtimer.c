c1ed693477a9686a39ad7a5c9c82d85b47408662
Geert Uytterhoeven <geert+renesas@glider.be>
2017-07-05 14:08:35 +0200

    BACKPORT: alarmtimer: Fix unavailable wake-up source in sysfs
    Currently the alarmtimer registers a wake-up source unconditionally,
    regardless of the system having a (wake-up capable) RTC or not.
    Hence the alarmtimer will always show up in
    /sys/kernel/debug/wakeup_sources, even if it is not available, and thus
    cannot be a wake-up source.
    
    To fix this, postpone registration until a wake-up capable RTC device is
    added.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Miroslav Lichvar <mlichvar@redhat.com>
    Cc: Richard Cochran <richardcochran@gmail.com>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: Stephen Boyd <stephen.boyd@linaro.org>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I3c3249e7a4c34b146280405f1ccd501012d53409

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -46,9 +46,9 @@ static struct alarm_base {
 static ktime_t freezer_delta;
 static DEFINE_SPINLOCK(freezer_delta_lock);
 
+#ifdef CONFIG_RTC_CLASS
 static struct wakeup_source *ws;
 
-#ifdef CONFIG_RTC_CLASS
 /* rtc timer and device for setting alarm wakeups at suspend */
 static struct rtc_timer		rtctimer;
 static struct rtc_device	*rtcdev;
@@ -92,10 +92,14 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 	unsigned long flags;
 	int err = 0;
 	struct rtc_device *rtc = to_rtc_device(dev);
+	struct wakeup_source *__ws;
+
 	if (rtcdev)
 		return -EBUSY;
 	if (!rtc->ops->set_alarm)
 		return -1;
+
+	__ws = wakeup_source_register("alarmtimer");
 
 	spin_lock_irqsave(&rtcdev_lock, flags);
 	if (!rtcdev) {
@@ -105,10 +109,15 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 		rtcdev = rtc;
 		/* hold a reference so it doesn't go away */
 		get_device(dev);
+		ws = __ws;
+		__ws = NULL;
 	}
 
 rtc_irq_reg_err:
 	spin_unlock_irqrestore(&rtcdev_lock, flags);
+
+	wakeup_source_unregister(__ws);
+
 	return err;
 
 }
@@ -914,7 +923,6 @@ static int __init alarmtimer_init(void)
 		error = PTR_ERR(pdev);
 		goto out_drv;
 	}
-	ws = wakeup_source_register("alarmtimer");
 	return 0;
 
 out_drv:
